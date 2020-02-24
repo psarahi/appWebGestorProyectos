465c78d149f7abbd7c3f5af793e78fd752d21fa2
psarahi <p.sarahi@unitec.edu>
2020-02-24 14:42:17 -0600

    add primeNG

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// PRIMENG
+import { AccordionModule } from 'primeng/accordion';     // accordion and accordion tab
+import { MenuItem } from 'primeng/api';                 // api
+import { CheckboxModule } from 'primeng/checkbox';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AccordionModule,
+    CheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
